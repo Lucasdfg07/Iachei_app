94bac9c3945d55df2edad3e22a5769246644965d
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-03-24 12:21:43 -0300

    modifying address relationship

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,3 @@
 class Address < ApplicationRecord
-	belongs_to :establishment
-
 	validates_presence_of :street, :neighborhood, :number
 end
--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -1,7 +1,6 @@
 class Establishment < ApplicationRecord
 	belongs_to :user
 	has_many :cities, through: :establishment_cities, dependent: :destroy
-	has_many :addresses, dependent: :destroy
 	belongs_to :category
 
 
